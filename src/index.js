8f6ac7c57b8b6ca9702d25552483d7146a8cb892
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-10-19 23:28:04 -0500

    about cat

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const breedSelect = document.querySelector('.breed-select');
+const infoCat = document.querySelector('.cat-info');
 
 function markup(response) {
   const joinOptions = response
     .map(element => `<option value="${element.id}">${element.name}</option>`)
     .join('');
   breedSelect.innerHTML = joinOptions;
+}
+
+function markupDetail(response) {
+  const catBreedObj = response[0].breeds[0];
+  const urlCat = response[0].url;
+  const information = `
+    <img src="${urlCat}" alt="" height="300px" width="400px">
+    <h1>${catBreedObj.name}</h1>
+      <p>${catBreedObj.description}</p>
+      <p> <b>Temperament: </b>${catBreedObj.temperament}</p>`;
+  console.log(information);
+  console.log((infoCat.innerHTML = information));
 }
 
 window.addEventListener('load', () => {
@@ -16,8 +29,8 @@ window.addEventListener('load', () => {
 });
 
 breedSelect.addEventListener('change', () => {
-  const selectedId = this.breedSelect;
+  const selectedId = breedSelect.value;
   fetchCatByBreed(selectedId)
-    .then(detail => console.log(detail))
+    .then(detail => markupDetail(detail))
     .catch(error => console.log(error));
 });
