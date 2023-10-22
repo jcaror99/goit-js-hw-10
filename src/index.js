3044f838aefece5733dcee81567871618eb37cd7
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-10-21 23:31:55 -0500

    message

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const breedSelect = document.querySelector('.breed-select');
 const infoCat = document.querySelector('.cat-info');
+const loaderInfo = document.querySelector('.loader');
+const errorInfo = document.querySelector('.error');
+breedSelect.style.display = 'none';
+errorInfo.style.display = 'none';
 
 function markup(response) {
   const joinOptions = response
@@ -14,6 +18,7 @@ function markupDetail(response) {
   const catBreedObj = response[0].breeds[0];
   const urlCat = response[0].url;
   const information = `
+  <br>
     <img src="${urlCat}" alt="" height="300px" width="400px">
     <h1>${catBreedObj.name}</h1>
       <p>${catBreedObj.description}</p>
@@ -24,13 +29,29 @@ function markupDetail(response) {
 
 window.addEventListener('load', () => {
   fetchBreeds()
-    .then(breeds => markup(breeds))
-    .catch(error => console.log(error));
+    .then(breeds => {
+      markup(breeds);
+      breedSelect.style.display = 'block';
+      loaderInfo.style.display = 'none';
+    })
+    .catch(error => {
+      errorInfo.style.display = 'block';
+      console.log(error);
+    });
 });
 
 breedSelect.addEventListener('change', () => {
+  loaderInfo.style.display = 'block';
+  infoCat.style.display = 'none';
   const selectedId = breedSelect.value;
   fetchCatByBreed(selectedId)
-    .then(detail => markupDetail(detail))
-    .catch(error => console.log(error));
+    .then(detail => {
+      markupDetail(detail);
+      infoCat.style.display = 'block';
+      loaderInfo.style.display = 'none';
+    })
+    .catch(error => {
+      errorInfo.style.display = 'block';
+      console.log(error);
+    });
 });
